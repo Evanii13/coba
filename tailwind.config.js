9249b9f4b0ed89f13221111e6a0c24884a25a153
Devani Zelvia <125328215+Evanii13@users.noreply.github.com>
2023-09-29 18:27:54 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './public/index.html',
+  ],
+  theme: {
+    extend: {
+      letterSpacing: {
+        ngempit:'-0.5em',
+        ngangkang: '.5em',
+      }
+    },
+  },
+  plugins: [],
+}
